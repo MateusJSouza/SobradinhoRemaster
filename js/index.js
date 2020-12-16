c24963f52eade5db32accd55780e22e7eb5ddbd4
Mateus <mat_azuos@outlook.com>
2020-12-15 22:44:40 -0300

    Acessibilidades pt1

---
+++ js/index.js
@@ -0,0 +1,72 @@
+(function () {
+    var Contrast = {
+        storage: 'contrastState',
+        cssClass: 'contrast',
+        currentState: null,
+        check: checkContrast,
+        getState: getContrastState,
+        setState: setContrastState,
+        toogle: toogleContrast,
+        updateView: updateViewContrast
+    };
+
+    window.toggleContrast = function () { Contrast.toogle(); };
+
+    Contrast.check();
+
+    function checkContrast() {
+        this.updateView();
+    }
+
+    function getContrastState() {
+        return localStorage.getItem(this.storage) === 'true';
+    }
+
+    function setContrastState(state) {
+        localStorage.setItem(this.storage, '' + state);
+        this.currentState = state;
+        this.updateView();
+    }
+
+    function updateViewContrast() {
+        var body = document.body;
+        
+        if (!body) return;
+
+        if (this.currentState === null)
+            this.currentState = this.getState();
+
+        if (this.currentState)
+            body.classList.add(this.cssClass);
+        else
+            body.classList.remove(this.cssClass);
+    }
+
+    function toogleContrast() {
+        this.setState(!this.currentState);
+    }
+
+})();
+
+function pesquisa(el) {
+    var display = document.getElementById(el).style.display;
+                if(display == "none")
+            document.getElementById(el).style.display = 'block';
+                else
+            document.getElementById(el).style.display = 'none';
+}
+
+const menuItems = document.querySelectorAll('.nav a');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector('#content').offsetTop;
+
+    window.scroll(0, to);
+}
